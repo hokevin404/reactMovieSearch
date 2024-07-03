1aea7877560ff81f7a50c4da8a31bfd340d063f4
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 15:38:33 -0700

    Built Form comp

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import './App.css';
 function App() {
 
   return (
-    <>
+    <div className='App'>
       <Form />
       <MovieDisplay />
-    </>
+    </div>
   )
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,11 @@
-function Form() {
+function Form(props) {
   return (
-    <div>Form</div>
+    <div>
+        <form>
+            <input type="text" />
+            <input type="submit" value={submit} />
+        </form>
+    </div>
   )
 }
 
