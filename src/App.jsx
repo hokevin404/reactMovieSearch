4de4a8b2a8aa00ba8a1e66b50a6bdd4bc140980a
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 15:36:46 -0700

    import comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
+import MovieDisplay from './components/MovieDisplay';
+import Form from './components/Form';
 import './App.css';
 
 function App() {
 
   return (
     <>
-      
+      <Form />
+      <MovieDisplay />
     </>
   )
 }
