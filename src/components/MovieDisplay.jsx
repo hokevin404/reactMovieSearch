26c186bfba9ccf6c9128c1bc42a9d7f4da2c5a8b
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 16:24:41 -0700

    Added loaded() in Movie comp

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -1,8 +1,19 @@
 
-function MovieDisplay() {
-  return (
-    <div>MovieDisplay</div>
-  )
-}
+function MovieDisplay({movie}) {
+  // Function to return loaded JSX
+  const loaded = () => {
+    return (
+      <>
+        <h1>{movie.Title}</h1>
+        <h2>{movie.Genre}</h2>
+        <img src={movie.poster} alt={movie.Title} />
+        <h2>{movie.Year}</h2>
+      </>
+    );
+  };
+
+};
+
+
 
 export default MovieDisplay
