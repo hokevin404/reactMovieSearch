78e3ac5dd41fe701b2979d9651926f084ad3c77f
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-03 15:57:56 -0700

    Added form handle()

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,21 @@
 function Form(props) {
-    const [formData, setFormData] = useState({searchterm: ''});
-    
-  return (
-    <div>
-        <form>
-            <input type="text" />
-            <input type="submit" value={submit} />
-        </form>
-    </div>
-  )
+    // State to hold form data from user input
+    const [formData, setFormData] = useState({ searchterm: '' });
+
+    // Function to update form
+    const handleChange = (event) => {
+        // Update form values using event object to obtain user values
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
+
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <input type="submit" value={submit} />
+            </form>
+        </div>
+    )
 }
 
 export default Form
